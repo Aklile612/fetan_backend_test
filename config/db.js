31649675c4070e8a2fc30099d2fe9501dbaa317e
Aklile Ansa <aklilemente72@gmail.com>
2025-07-09 22:41:13 +0300

    database connection written

---
+++ config/db.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const db_URI = process.env.db_URI
+
+const dbConnect= async()=>{
+    try {
+        await mongoose.connect(db_URI)
+        console.log("Connected")
+    } catch (error) {
+        console.log(error.message)
+        process.exit(1)
+    }
+}
+
+export default dbConnect
