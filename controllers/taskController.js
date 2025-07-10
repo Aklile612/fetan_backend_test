4ecde927856adba707646b5a43c47593272088ad
Aklile Ansa <aklilemente72@gmail.com>
2025-07-10 14:25:18 +0300

    added the update and deletetask

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -20,3 +20,40 @@ export const  addTask=async (req,res)=>{
         
     }
 }
+
+
+// edit task pending-> finished
+
+export const editTask=async (req,res)=>{
+  const id= req.params.id
+  try {
+    
+    const editT= await Task.findById(id)
+    // console.log(editT)
+
+    if (editT.status==="Pending"){
+      editT.status="Completed"
+      await editT.save()
+      return res.status(200).json({ message: "Task marked as completed",  editT });
+
+    }else{
+      editT.status="Pending"
+      await editT.save()
+      return res.status(200).json({message: "Task marked as Pending",  editT });
+    }
+  } catch (error) {
+    console.log(error.message)
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({message:"Please try again, there might be an unknown error on the server",error})
+  }
+}
+
+export const deleteTask= async(req,res)=>{
+  const id = req.params.id
+  try {
+    const deleteT = await Task.findByIdAndDelete(id)
+    return res.status(StatusCodes.OK).json({message:" Task successfully deleted"})
+  } catch (error) {
+    console.log(error.message)
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({message:"Please try again, there might be an unknown error on the server",error})
+  }
+}
