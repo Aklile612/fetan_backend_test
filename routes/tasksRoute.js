8933ab953a972bf39d2212dc36ba35074ad32b4b
Aklile Ansa <aklilemente72@gmail.com>
2025-07-10 17:57:04 +0300

    added the edit task route

--- routes/tasksRoute.js
+++ routes/tasksRoute.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { addTask, deleteTask, editTask } from "../controllers/taskController.js";
+import { addTask, deleteTask, editTask, getTask } from "../controllers/taskController.js";
 
 
 const router=Router()
 
 router.post('/addtask',addTask)
+router.get('/gettask',getTask)
 router.put('/edittask/:id',editTask)
 router.delete('/deletetask/:id',deleteTask)
 
