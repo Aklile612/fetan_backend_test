7e06de787fcf4ffd3a897ba19d7f8e4ce619bc23
Aklile Ansa <aklilemente72@gmail.com>
2025-07-10 14:25:47 +0300

    added the updata and delete route

--- routes/tasksRoute.js
+++ routes/tasksRoute.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { addTask } from "../controllers/taskController.js";
+import { addTask, deleteTask, editTask } from "../controllers/taskController.js";
 
 
 const router=Router()
 
 router.post('/addtask',addTask)
+router.put('/edittask/:id',editTask)
+router.delete('/deletetask/:id',deleteTask)
 
 export default router
