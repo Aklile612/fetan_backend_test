21dbd0663771077a334a383c0553e1b311c9e888
Aklile Ansa <aklilemente72@gmail.com>
2025-07-10 11:36:43 +0300

    added the route and middleware

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 import express from 'express'
 import cors from 'cors'
 import auth from "./routes/auth.js"
+import userRouter from "./routes/userRoute.js"
+import { authMiddleware } from "./middlewares/authMiddleware.js";
+
 const app=express()
 
 
@@ -9,6 +12,8 @@ app.use(express.json())
 
 
 app.use("/auth",auth)
+app.use("/user",authMiddleware,userRouter)
+
 
 
 export default app
